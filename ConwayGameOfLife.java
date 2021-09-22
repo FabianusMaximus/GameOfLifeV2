b801b6b1df378bb795eeb48f198f19ee7f4abc59
FabianusMaximus <leppfabian@gmail.com>
2021-09-22 18:22:59 +0200

    GameOfLifeV2 erstellt

--- ConwayGameOfLife.java
+++
@@ -1,8 +0,0 @@
-package Version_2;
-
-public class ConwayGameOfLife {
-    public static void main(String[] args) {
-        Control c = new Control();
-        c.start();
-    }
-}
---
+++ src/ConwayGameOfLife.java
@@ -0,0 +1,8 @@
+package Version_2.src;
+
+public class ConwayGameOfLife {
+    public static void main(String[] args) {
+        Control c = new Control();
+        c.start();
+    }
+}
