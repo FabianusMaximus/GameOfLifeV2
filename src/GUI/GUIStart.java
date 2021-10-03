5f6f2736a1c57a8f80d4332baf3c8463236e5470
FabianusMaximus <leppfabian@gmail.com>
2021-10-03 20:14:24 +0200

    GUI updated

--- src/GUI/GUIStart.java
+++ src/GUI/GUIStart.java
@@ -11,15 +11,40 @@ public class GUIStart extends JFrame implements ActionListener {
     private JButton btnRandomStatGen;
     private JButton btnUserInputGen;
     private JLabel lbWorld;
+    private JLabel lbBreite;
+    private JLabel lbHoehe;
+    private JTextField tfBreite;
+    private JTextField tfHoehe;
+    private JCheckBoxMenuItem cbmiEinzeln;
+    private JCheckBoxMenuItem cbmiMehrere;
+    private JTextField tfAnzGeneration;
 
     public GUIStart(Control pControll){
         setTitle("Conway's Game of Life");
         Container cp = getContentPane();
         ((Container) cp).setLayout(null);
-        lbWorld = new JLabel("Größe der Generation: ");
+        lbWorld = new JLabel("Größe der Generation:");
         lbWorld.setBounds(20, 30, 140, 20);
         cp.add(lbWorld);
+
+        lbBreite = new JLabel("Breite");
+        lbBreite.setBounds(170, 30, 50, 20);
+        cp.add(lbBreite);
+
+        tfBreite = new JTextField("");
+        tfBreite.setBounds(215,30,50,20);
+        cp.add(tfBreite);
+
+        lbHoehe = new JLabel("Hoehe");
+        lbHoehe.setBounds(280, 30, 50, 20);
+        cp.add(lbHoehe);
+
+        tfHoehe = new JTextField("");
+        tfHoehe.setBounds(330,30,50,20);
+        cp.add(tfHoehe);
+
         //TODO hier fehlt noch was
+        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(new Dimension(400, 250));
         setResizable(false);
